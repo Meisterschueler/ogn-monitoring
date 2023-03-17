99824d59c86bcb9c835a65de793d65da3167065d
Meisterschueler <kammermark@gmx.de>
2023-03-17 22:34:05 +0100

    Change backend db from sqlite3 to postgres

--- sql/timescaledb-initdb/900_user_permissions.sql
+++ sql/timescaledb-initdb/900_user_permissions.sql
@@ -1,8 +1,13 @@
---- create read-only user for grafana
---DROP OWNED BY grafanareader;
+--- create read-only user for grafana (frontend)
 DROP USER IF EXISTS grafanareader;
-
 CREATE USER grafanareader WITH PASSWORD 'password';
+
 GRANT USAGE ON SCHEMA public TO grafanareader;
---GRANT SELECT ON public.receivers TO grafanareader;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafanareader;
+
+--- create user for grafana (backend) and give complete access to database 'grafana'
+DROP USER IF EXISTS grafana;
+CREATE USER grafana WITH PASSWORD 'password';
+
+CREATE DATABASE grafana;
+ALTER DATABASE grafana OWNER TO grafana;
