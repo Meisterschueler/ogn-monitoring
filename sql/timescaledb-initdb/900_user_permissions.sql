58073e0415bb86a4e33ed686e2c4438d5d9d26c8
Konstantin Gründger <kammermark@gmx.de>
2023-05-05 07:03:20 +0000

    Lets crashs if user already exists

--- sql/timescaledb-initdb/900_user_permissions.sql
+++ sql/timescaledb-initdb/900_user_permissions.sql
@@ -1,12 +1,10 @@
 --- create read-only user for grafana (frontend)
-DROP USER IF EXISTS grafanareader;
 CREATE USER grafanareader WITH PASSWORD 'password';
 
 GRANT USAGE ON SCHEMA public TO grafanareader;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO grafanareader;
 
 --- create user for grafana (backend) and give complete access to database 'grafana'
-DROP USER IF EXISTS grafana;
 CREATE USER grafana WITH PASSWORD 'password';
 
 CREATE DATABASE grafana;
