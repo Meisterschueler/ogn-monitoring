9488bcd6a0e175a377fcc56181ff5adddaefee78
Konstantin Gründger <kammermark@gmx.de>
2023-05-24 08:36:17 +0000

    Execute scripts initially

--- scripts/entrypoint_cron.sh
+++ scripts/entrypoint_cron.sh
@@ -2,6 +2,12 @@
 
 echo === Started entrypoint_cron.sh ===
 
+# Initial execution of the scripts
+sh /scripts/update_ddb.sh
+sh /scripts/update_opensky.sh
+sh /scripts/update_weglide.sh
+
+# Configure cron for repeated execution
 echo "@daily /scripts/update_ddb.sh" >> /var/spool/cron/crontabs/root
 echo "@daily /scripts/update_opensky.sh" >> /var/spool/cron/crontabs/root
 echo "@daily /scripts/update_weglide.sh" >> /var/spool/cron/crontabs/root
