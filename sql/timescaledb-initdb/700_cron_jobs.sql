89ff0633faa4fa2505520fcd9738fb799ac44fac
Meisterschueler <kammermark@gmx.de>
2023-05-25 13:26:19 +0200

    Call receiver/sender updates consecutive to prevent deadlocks

--- sql/timescaledb-initdb/700_cron_jobs.sql
+++ sql/timescaledb-initdb/700_cron_jobs.sql
@@ -1,9 +1,5 @@
 -- 'Continuous' updates
-SELECT cron.schedule('*/5 * * * *', 'SELECT update_receivers();');
-SELECT cron.schedule('*/5 * * * *', 'SELECT update_senders();');
-
-SELECT cron.schedule('*/5 * * * *', 'SELECT update_receiver_countries();');
-
+SELECT cron.schedule('*/5 * * * *', 'SELECT update_receivers(); SELECT update_senders(); SELECT update_receiver_countries();');
 SELECT cron.schedule('*/5 * * * *', 'REFRESH MATERIALIZED VIEW senders_joined;');
 
 -- 'Daily' updates
