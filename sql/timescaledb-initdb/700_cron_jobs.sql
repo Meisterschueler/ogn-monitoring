9193924d844d51020ef3b9cc3224a3d2f343ba35
Meisterschueler <kammermark@gmx.de>
2023-05-25 09:23:27 +0200

    Refresh senders_relative_qualities daily

--- sql/timescaledb-initdb/700_cron_jobs.sql
+++ sql/timescaledb-initdb/700_cron_jobs.sql
@@ -1,6 +1,10 @@
+-- 'Continuous' updates
 SELECT cron.schedule('*/5 * * * *', 'SELECT update_receivers();');
 SELECT cron.schedule('*/5 * * * *', 'SELECT update_senders();');
 
 SELECT cron.schedule('*/5 * * * *', 'SELECT update_receiver_countries();');
 
 SELECT cron.schedule('*/5 * * * *', 'REFRESH MATERIALIZED VIEW senders_joined;');
+
+-- 'Daily' updates
+SELECT cron.schedule('10 0 * * *', 'REFRESH MATERIALIZED VIEW senders_relative_qualities;');
