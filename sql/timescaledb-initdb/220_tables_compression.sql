e6e85a051636af12bbec3f2b4709812d5f389076
Meisterschueler <kammermark@gmx.de>
2023-05-26 23:36:23 +0200

    Add compression for table invalids

--- sql/timescaledb-initdb/220_tables_compression.sql
+++ sql/timescaledb-initdb/220_tables_compression.sql
@@ -11,3 +11,9 @@ ALTER TABLE statuses SET (
 	timescaledb.compress_chunk_time_interval = '24 hours'
 );
 SELECT add_compression_policy('statuses', INTERVAL '2 days');
+
+ALTER TABLE invalids SET (
+	timescaledb.compress,
+	timescaledb.compress_chunk_time_interval = '24 hours'
+);
+SELECT add_compression_policy('invalids', INTERVAL '2 days');
