9367737d4c326e810bffff24539e952f5029c283
Meisterschueler <kammermark@gmx.de>
2023-05-26 15:36:36 +0200

    Add cron policy for receivers_joined

--- sql/timescaledb-initdb/700_cron_jobs.sql
+++ sql/timescaledb-initdb/700_cron_jobs.sql
@@ -1,6 +1,7 @@
 -- 'Continuous' updates
 SELECT cron.schedule('*/5 * * * *', 'SELECT update_receivers(); SELECT update_senders(); SELECT update_receiver_countries();');
 SELECT cron.schedule('*/5 * * * *', 'REFRESH MATERIALIZED VIEW senders_joined;');
+SELECT cron.schedule('*/5 * * * *', 'REFRESH MATERIALIZED VIEW receivers_joined;');
 
 -- 'Daily' updates
 SELECT cron.schedule('10 0 * * *', 'REFRESH MATERIALIZED VIEW senders_relative_qualities;');
