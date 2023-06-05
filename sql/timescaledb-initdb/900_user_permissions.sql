eebb481ac67e97083c4e641f18e1d739d36a8e58
Konstantin Gründger <kammermark@gmx.de>
2023-06-05 22:24:12 +0000

    Add telegraf

--- sql/timescaledb-initdb/900_user_permissions.sql
+++ sql/timescaledb-initdb/900_user_permissions.sql
@@ -9,3 +9,9 @@ CREATE USER grafana WITH PASSWORD 'password';
 
 CREATE DATABASE grafana;
 ALTER DATABASE grafana OWNER TO grafana;
+
+--- create user for telegraf (backend) and give complete access to database 'telegraf'
+CREATE USER telegraf WITH PASSWORD 'password';
+
+CREATE DATABASE telegraf;
+ALTER DATABASE telegraf OWNER TO telegraf;
