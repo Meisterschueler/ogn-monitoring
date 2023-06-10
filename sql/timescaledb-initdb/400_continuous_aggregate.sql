99e2b1562ccb3f9a02a0baf774205a394b38d31a
Konstantin Gründger <kammermark@gmx.de>
2023-06-10 17:04:31 +0200

    Take plausibility into account

--- sql/timescaledb-initdb/400_continuous_aggregate.sql
+++ sql/timescaledb-initdb/400_continuous_aggregate.sql
@@ -5,23 +5,16 @@ AS
 SELECT
 	time_bucket('5 minutes', ts) AS ts,
 	src_call,
+	dst_call,
 	receiver,
-
-	CASE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND bearing IS NOT NULL AND course IS NOT NULL AND COALESCE(speed, 0) >= 5 AND ABS(COALESCE(climb_rate, 0)) < 2000 AND ABS(COALESCE(turn_rate, 0)) * speed < 30
-		WHEN FALSE THEN NULL
-		ELSE CAST(((CAST(bearing AS INTEGER) + 15 + 180) % 360) / 30 AS INTEGER) * 30
-	END AS radial,
-	CASE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND bearing IS NOT NULL AND course IS NOT NULL AND COALESCE(speed, 0) >= 5 AND ABS(COALESCE(climb_rate, 0)) < 2000 AND ABS(COALESCE(turn_rate, 0)) * speed < 30
-		WHEN FALSE THEN NULL
-		ELSE CAST(((CAST(bearing AS INTEGER) + 15 - course + 360) % 360) / 30 AS INTEGER) * 30
-	END AS relative_bearing,
+	plausibility,
 
 	FIRST(ts, ts) AS first_position,
 	LAST(ts, ts) AS last_position,
-	MAX(normalized_quality) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000) AS normalized_quality,
-	MAX(distance) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000) AS distance,
-	MIN(altitude) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND altitude IS NOT NULL) AS altitude_min,
-	MAX(altitude) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND altitude IS NOT NULL) AS altitude_max,
+	MAX(normalized_quality) AS normalized_quality,
+	MAX(distance) AS distance,
+	MIN(altitude) AS altitude_min,
+	MAX(altitude) AS altitude_max,
 
 	COUNT(*) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND bearing IS NOT NULL AND course IS NOT NULL AND COALESCE(speed, 0) >= 5 AND (ABS(COALESCE(climb_rate, 0)) >= 2000 OR ABS(COALESCE(turn_rate, 0)) * speed >= 30)) AS points_dynamic,
 	COUNT(*) FILTER (WHERE COALESCE(error, 0) <= 5 AND COALESCE(normalized_quality, 0) <= 50 AND COALESCE(distance, 0) <= 640000 AND bearing IS NOT NULL AND course IS NOT NULL AND COALESCE(speed, 0) >= 5 AND ABS(COALESCE(climb_rate, 0)) < 2000 AND ABS(COALESCE(turn_rate, 0)) * speed < 30) AS points_motion,
@@ -34,43 +27,18 @@ WHERE
 	src_call NOT LIKE 'RND%'
 	AND dst_call IN ('APRS', 'OGFLR', 'OGNFNT', 'OGNTRK')
 	AND receiver NOT LIKE 'GLIDERN%'
-GROUP BY time_bucket('5 minutes', ts), src_call, receiver, radial, relative_bearing
+GROUP BY 1, 2, 3, 4, 5
 WITH NO DATA;
 
--- ranking statistics 5m
-CREATE MATERIALIZED VIEW ranking_statistics_5m
-WITH (timescaledb.continuous, timescaledb.materialized_only = TRUE)
-AS
-SELECT
-	time_bucket('5 minutes', ts) AS ts,
-	src_call,
-	receiver,
-
-	FIRST(first_position, ts) AS first_position,
-	LAST(last_position, ts) AS last_position,
-	MAX(normalized_quality) AS normalized_quality,
-	MAX(distance) AS distance,
-	MIN(altitude_min) AS altitude_min,
-	MAX(altitude_max) AS altitude_max,
-
-	SUM(points_dynamic) AS points_dynamic,
-	SUM(points_motion) AS points_motion,
-	SUM(points_static) AS points_static,
-	SUM(points_fake) AS points_fake,
-	SUM(points_error) AS points_error,
-	SUM(points_total) AS points_total
-
-FROM positions_5m AS s5m
-GROUP BY time_bucket('5 minutes', ts), src_call, receiver
-WITH NO DATA;
-
-CREATE MATERIALIZED VIEW ranking_statistics_1h
+CREATE MATERIALIZED VIEW positions_1h
 WITH (timescaledb.continuous, timescaledb.materialized_only = TRUE)
 AS
 SELECT
 	time_bucket('1 hour', ts) AS ts,
 	src_call,
+	dst_call,
 	receiver,
+	plausibility,
 
 	FIRST(first_position, ts) AS first_position,
 	LAST(last_position, ts) AS last_position,
@@ -86,17 +54,19 @@ SELECT
 	SUM(points_error) AS points_error,
 	SUM(points_total) AS points_total
 
-FROM ranking_statistics_5m AS rs5m
-GROUP BY time_bucket('1 hour', ts), src_call, receiver
+FROM positions_5m
+GROUP BY 1, 2, 3, 4, 5
 WITH NO DATA;
 
-CREATE MATERIALIZED VIEW ranking_statistics_1d
+CREATE MATERIALIZED VIEW positions_1d
 WITH (timescaledb.continuous)
 AS
 SELECT
 	time_bucket('1 day', ts) AS ts,
 	src_call,
+	dst_call,
 	receiver,
+	plausibility,
 
 	FIRST(first_position, ts) AS first_position,
 	LAST(last_position, ts) AS last_position,
@@ -112,8 +82,8 @@ SELECT
 	SUM(points_error) AS points_error,
 	SUM(points_total) AS points_total
 
-FROM ranking_statistics_1h AS rs1h
-GROUP BY time_bucket('1 day', ts), src_call, receiver
+FROM positions_1h
+GROUP BY 1, 2, 3, 4, 5
 WITH NO DATA;
 
 CREATE MATERIALIZED VIEW quality_statistics_1d
@@ -134,7 +104,8 @@ SELECT
 	percentile_disc(0.7) WITHIN GROUP (order by normalized_quality) AS percentile_70,
 	percentile_disc(0.8) WITHIN GROUP (order by normalized_quality) AS percentile_80,
 	percentile_disc(0.9) WITHIN GROUP (order by normalized_quality) AS percentile_90
-FROM ranking_statistics_1d
+
+FROM positions_1d
 WHERE normalized_quality IS NOT NULL AND points_motion > 10
 GROUP BY time_bucket('1 day', ts), receiver
 WITH NO DATA;
@@ -147,23 +118,15 @@ SELECT
 	time_bucket('1 day', ts) AS ts,
 	src_call,
 	receiver,
+	plausibility,
 	radial,
 	relative_bearing,
 
-	FIRST(first_position, ts) AS first_position,
-	LAST(last_position, ts) AS last_position,
 	MAX(normalized_quality) AS normalized_quality,
 	MAX(distance) AS distance,
-	MIN(altitude_min) AS altitude_min,
-	MAX(altitude_max) AS altitude_max,
 
-	SUM(points_dynamic) AS points_dynamic,
-	SUM(points_motion) AS points_motion,
-	SUM(points_static) AS points_static,
-	SUM(points_fake) AS points_fake,
-	SUM(points_error) AS points_error,
 	SUM(points_total) AS points_total
 
-FROM positions_5m AS p5m
-GROUP BY time_bucket('1 day', ts), src_call, receiver, radial, relative_bearing
+FROM direction_statistics_1h
+GROUP BY 1, 2, 3, 4, 5, 6
 WITH NO DATA;
--- sql/timescaledb-initdb/410_policies.sql
+++ sql/timescaledb-initdb/410_policies.sql
@@ -1,29 +1,31 @@
 SELECT add_continuous_aggregate_policy('positions_5m',
-  start_offset => INTERVAL '1 hour',
-  end_offset => INTERVAL'5 minutes',
+  start_offset => INTERVAL '10 minutes',
+  end_offset => NULL,
+  initial_start => '2000-01-01 00:00:00'::TIMESTAMPTZ,
   schedule_interval => INTERVAL'5 minutes');
+ALTER MATERIALIZED VIEW positions_5m SET (timescaledb.compress = true);
+SELECT add_compression_policy('positions_5m', compress_after => INTERVAL '15 minutes');
 
-SELECT add_continuous_aggregate_policy('ranking_statistics_5m',
-  start_offset => INTERVAL '1 hour',
-  end_offset => INTERVAL '5 minutes',
-  schedule_interval => INTERVAL '5 minutes');
-
-SELECT add_continuous_aggregate_policy('ranking_statistics_1h',
-  start_offset => INTERVAL '12 hours',
-  end_offset => INTERVAL '1 hour',
+SELECT add_continuous_aggregate_policy('positions_1h',
+  start_offset => INTERVAL '2 hours',
+  end_offset => NULL,
+  initial_start => '2000-01-01 00:05:00'::TIMESTAMPTZ,
   schedule_interval => INTERVAL '1 hour');
+ALTER MATERIALIZED VIEW positions_1h SET (timescaledb.compress = true);
+SELECT add_compression_policy('positions_1h', compress_after => INTERVAL '3 hours');
 
-SELECT add_continuous_aggregate_policy('ranking_statistics_1d',
-  start_offset => INTERVAL '10 days',
-  end_offset => INTERVAL '1 day',
+SELECT add_continuous_aggregate_policy('positions_1d',
+  start_offset => INTERVAL '2 days',
+  end_offset => NULL,
+  initial_start => '2000-01-01 00:10:00'::TIMESTAMPTZ,
   schedule_interval => INTERVAL '1 day');
-
-SELECT add_continuous_aggregate_policy('quality_statistics_1d',
-  start_offset => INTERVAL '10 days',
-  end_offset => INTERVAL '1 day',
-  schedule_interval => INTERVAL '1 day');
+ALTER MATERIALIZED VIEW positions_1d SET (timescaledb.compress = true);
+SELECT add_compression_policy('positions_1d', compress_after => INTERVAL '3 days');
 
 SELECT add_continuous_aggregate_policy('direction_statistics_1d',
-  start_offset => INTERVAL '10 days',
-  end_offset => INTERVAL '1 day',
+  start_offset => INTERVAL '2 days',
+  end_offset => NULL,
+  initial_start => '2000-01-01 00:20:00'::TIMESTAMPTZ,
   schedule_interval => INTERVAL '1 day');
+ALTER MATERIALIZED VIEW direction_statistics_1d SET (timescaledb.compress = true);
+SELECT add_compression_policy('direction_statistics_1d', compress_after => INTERVAL '3 days');
