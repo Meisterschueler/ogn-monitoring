2586d7faad8a9bd6590e5e7ad0dfb139ad372097
Konstantin Gründger <kammermark@gmx.de>
2023-06-17 08:35:09 +0200

    Uncouple plausibilsation

--- sql/timescaledb-initdb/700_cron_jobs.sql
+++ sql/timescaledb-initdb/700_cron_jobs.sql
@@ -1,10 +1,12 @@
 -- 'Continuous' updates
-SELECT cron.schedule('*/5 * * * *', '-- regular 5min. update
+SELECT cron.schedule('*/5 * * * *', '-- 5min. update senders + receivers
     SELECT update_receivers();
     SELECT update_senders();
     SELECT update_receiver_countries();
+');
+SELECT cron.schedule('*/10 * * * *', '-- 10min. plausibilisation
     SELECT update_plausibilities(
-		(NOW()-INTERVAL''11 minutes'')::TIMESTAMP, 
+		(NOW()-INTERVAL''16 minutes'')::TIMESTAMP, 
 		(NOW()-INTERVAL''5 minutes'')::TIMESTAMP
 	);
 ');
