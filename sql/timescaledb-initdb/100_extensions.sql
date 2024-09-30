2cdd83ae53cc5a18516618f810a09b514b8aca62
Meisterschueler <kammermark@gmx.de>
2024-09-30 18:20:08 +0200

    disable tuple decompression limit

--- sql/timescaledb-initdb/100_extensions.sql
+++ sql/timescaledb-initdb/100_extensions.sql
@@ -9,3 +9,6 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 
 -- activate telemetry for TimescaleDB
 ALTER DATABASE "ogn" SET timescaledb.telemetry_level = 'basic';
+
+-- set tuple decompression limit to unlimited (==0)
+SET timescaledb.max_tuples_decompressed_per_dml_transaction TO 0
