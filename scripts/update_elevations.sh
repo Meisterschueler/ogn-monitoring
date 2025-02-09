7bfce130bf60711b02e828e421891a82ac012375
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-02-09 01:42:30 +0100

    Fix Docker path in update_elevations

--- scripts/update_elevations.sh
+++ scripts/update_elevations.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+test ! -f /ressources/tiles/N35E129.hgt || exit 0
+
 echo --- Started elevation import ---
 
 echo Download tile files
-cat tiles.txt | xargs wget -P /tiles
+mkdir -p /ressources/tiles
+cat tiles.txt | xargs wget -P /ressources/tiles
 
 echo Unzipping files
-find /tiles/*.zip | xargs -n 1 unzip -j -d /tiles
+find /ressources/tiles/*.zip | xargs -n 1 unzip -j -d /ressources/tiles
 
 echo Import elevation data into table 'elevations'
-raster2pgsql -a -e -s 4326 -t 100x100 -Y /tiles/*.hgt elevations | PGPASSWORD=${POSTGRES_PASSWORD} psql --user=${POSTGRES_USER} --host=timescaledb --dbname=${POSTGRES_DB}
+raster2pgsql -a -e -s 4326 -t 100x100 -Y /ressources/tiles/*.hgt elevations | PGPASSWORD=${POSTGRES_PASSWORD} psql --user=${POSTGRES_USER} --host=timescaledb --dbname=${POSTGRES_DB}
 
 echo --- Finished elevation import ---
